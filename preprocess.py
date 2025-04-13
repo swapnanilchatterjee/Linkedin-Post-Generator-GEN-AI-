6429d7e43bdacb0a0d9bf4222758229a4a3d502d
Swapnanil Chatterjee <126894966+swapnanilchatterjee@users.noreply.github.com>
2025-04-13 19:26:54 +0530

    preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -82,4 +82,4 @@ def get_unified_tags(posts_with_metadata):
 
 
 if __name__ == "__main__":
-    process_posts("data/raw_posts.json", "data/processed_posts.json")
+    process_posts("Data/raw_posts.json", "Data/processed_posts.json")
